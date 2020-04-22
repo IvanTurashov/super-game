19cf20fbf1648ad4561e0cb87cb9fada895ad593
Ivan Votchel <votchel@reg.ru>
2020-04-22 20:15:43 +0700

    add core class

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -6,16 +6,20 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from '@vue/composition-api';
+import Game from '@/Game/CoreGame';
+import { CoreGame } from '@/types/Core';
 
 export default defineComponent({
   name: 'Game',
   setup() {
     const canvas = ref<HTMLCanvasElement | null>(null);
+    const game = ref<CoreGame>(null);
 
     const init = () => {
-      if (canvas.value) {
-        canvas.value.width = 500;
-        canvas.value.height = 500;
+      const ctx = canvas.value && canvas.value.getContext('2d');
+      if (ctx) {
+        game.value = new Game(ctx, { width: 500, height: 500 });
+        game.value.render();
       }
     };
     onMounted(() => {
---
+++ src/types/Core.ts
@@ -0,0 +1,8 @@
+export interface CoreGame {
+  render(): void;
+}
+
+export interface GameConfig {
+   width: number;
+  height: number;
+}
