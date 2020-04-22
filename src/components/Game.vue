fd61a6e7b667ff9bdd59cba6f3593cf64b19bfa3
Ivan Turashov <ivan.turashov@yandex.ru>
2020-04-22 10:17:24 +0300

    added Game

---
+++ src/components/Game.vue
@@ -0,0 +1,27 @@
+<template>
+  <canvas></canvas>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Game extends Vue {
+  mounted() {
+    this.init();
+  }
+
+  init(): void {
+    const canvas = this.$el;
+
+    canvas.width = this.$parent.$el.clientWidth;
+    canvas.height = this.$parent.$el.clientHeight;
+  }
+}
+</script>
+
+<style scoped>
+canvas {
+  background-color: antiquewhite;
+}
+</style>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/Game.vue';
 
-describe('HelloWorld.vue', () => {
+describe('Game.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
