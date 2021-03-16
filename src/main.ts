8dd0ecea7b30cb5ebf479c1ffec8a32f8ad4d2ef
anarkiVan <votchel.ivaninq@gmail.com>
2021-03-16 22:47:11 +0700

    return build production for minimizer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
 
